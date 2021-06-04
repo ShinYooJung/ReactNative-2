eb6c9a8bdfa7dfde5fc6a9e1efa1a29ced845332
YuMi <92506_@naver.com>
2021-06-04 22:17:31 +0900

    27. TouchableOpacity

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
@@ -21,11 +21,16 @@ export default class App extends React.Component {
             });
           });
       }
+
+  onPressCity(item) {
+  console.log('onPressCity =', item);
+  }
+    
   renderItem(city) {
     return (
-      <View style={styles.item}>
-        <Text style={styles.text}>{city}</Text>
-      </View>
+        <TouchableOpacity style={styles.item} onPress={this.onPressCity}>
+            <Text style={styles.text}>{city}</Text>
+        </TouchableOpacity>
     );
   }
 
