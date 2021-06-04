d3f184937767593204b14ede954f45d06bd9bfa2
YuMi <92506_@naver.com>
2021-06-04 22:42:15 +0900

    30. React-Navigation

--- App.js
+++ App.js
@@ -1,29 +1,38 @@
+import * as React from 'react';
+import 'react-native-gesture-handler';
 
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import CityList from './CityList';
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
-export default class App extends React.Component {
-   
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <CityList/>
+    <StatusBar style="auto" />
+  </View>
+);
 
-  render() {
-    return (
-      <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <CityList/>
-
-        <StatusBar style="auto" />
-      </SafeAreaView>
-      </SafeAreaProvider>
-    );
-  }
-}
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
- 
-});
+});s
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
