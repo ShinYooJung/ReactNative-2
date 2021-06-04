dab5f2033446504e323f24f12dff21e911a7539b
ShinYooJung <shinyoojung0@gmail.com>
2021-06-04 16:06:01 +0900

    02.Style: big text

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <Text style={styles.textBig}>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textBig: {
+    fontSize: 32
+  },
 });
+
